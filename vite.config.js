23a932f79e5efc05d43b164ab1b7270e7c4048dd
Timur Kuchin <kuchin.timur@bk.ru>
2025-05-29 22:52:16 +0700

    Some changes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    port: 8443,
-    host: '0.0.0.0',
-    https: {
-      cert: './server.crt',
-      key: './server.key'
-    }
-  },
+  base: '/',
   build: {
     outDir: 'dist'
   }
